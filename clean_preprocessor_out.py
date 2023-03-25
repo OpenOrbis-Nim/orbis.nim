bd1370bb32a57ff3e8d059fe1403e0ebae29011a
ac2pic <ac2pic@gmail.com>
2023-03-24 23:35:56 -0500

    Feature(clean_preprocessor_out.py): Move comments at end of macro line to line before

--- clean_preprocessor_out.py
+++ clean_preprocessor_out.py
@@ -28,8 +28,64 @@ def isMacroFunction(line):
 
 assert isMacroFunction("#define A(x) x") == True
 
+def isMacroWithComment(line):
+    if line[0] != "#":
+        return False
+    return line.find("//") != -1 or line.find("/*") != -1
+
+assert isMacroWithComment("#define A B /*My comment*/") == True
+
+
+def getMultilineCommentRange(line):
+    idx = line.find("/*")
+    if idx == -1:
+        return (-1,0)
+    startIdx = idx
+    idx += 2
+    endIdx = line.find("*/", idx)
+    if endIdx == -1:
+        return (-1, 0)
+    endIdx += 2
+    return (startIdx, endIdx - startIdx)
+
+def getSingleLineCommentRange(line):
+    idx = line.find("//")
+    if idx == -1:
+        return (-1, 0)
+    return (idx, len(line) - idx)
+
+def getCommentRange(line):
+    ml = getMultilineCommentRange(line)
+    sl = getSingleLineCommentRange(line)
+
+    if sl[0] == -1:
+        return ml
+
+    if ml[0] == -1:
+        return sl
+
+    if ml[0] < sl[0]:
+        return ml
+    return sl
+
+assert getCommentRange("/**/") == (0,4)
+assert getCommentRange("//") == (0,2)
+def fixMacroLineWithComment(line):
+    comment_range = getCommentRange(line)
+    if comment_range == (-1, 0):
+        return line
+    reference_line = ""
+    while (comment_range := getCommentRange(line)) != (-1, 0):
+        start,length = comment_range
+        reference_line += line[start:start+length] + "\n"
+        line = line[:start] + line[start+length:]
+    return reference_line + line
+assert fixMacroLineWithComment("#define A B /*My comment*/") == "/*My comment*/\n#define A B "
+assert fixMacroLineWithComment("#define A B //My comment") == "//My comment\n#define A B "
+
 filename = sys.argv[1]
 header = sys.argv[2]
+
 with open(filename, 'r') as fh:
     lines = [line for line in fh]    
 
@@ -46,6 +102,8 @@ while idx < lines_length:
     marker = parseLineMarker(line.strip())
     if marker:
         ignore_lines = marker[1] != header
+    elif isMacroWithComment(line):
+        out.append(fixMacroLineWithComment(line))                
     elif not ignore_lines and not isMacroFunction(line):
         out.append(line)
     idx += 1
