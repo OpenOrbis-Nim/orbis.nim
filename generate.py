c024ca7c4680e389712c697361c3d1a5867223ce
ac2pic <ac2pic@gmail.com>
2023-03-25 02:07:02 -0500

    Add various flags to make output nim files more valid

--- generate.py
+++ generate.py
@@ -24,28 +24,28 @@ def cleanPreprocessorOutput(infile, headername):
     subprocess.run(["./clean_preprocessor_out.py", infile, headername])
 
 
-def runC2Nim(infile):
+def runC2Nim(infile, baseHeaderFile):
     try:
         os.remove(infile[0:-2] + ".nim")
     except OSError:
         pass
-    subprocess.run(["c2nim", "--reordercomments", "--stdints", infile])
-
-def exportToDist():
-    args = "find ./source -name '*.nim' -exec cp -prv '{}' './dist/' ';'".split()
+    args = ["c2nim", "--cdecl",  "--reordercomments", "--stdints"]
+    args += ["--header:\"{}\"".format(baseHeaderFile)]
+    args += [infile]
     subprocess.run(args)
 
 for scrub_file in scrub_files:
     infile = scrub_file
     outfile = scrub_file[0:-2] + ".i"
     nimfile = scrub_file[0:-2] + ".nim"
+    libfile_header = scrub_file.replace("./source", "orbis")
     try:
         os.remove(outfile)
     except OSError:
         pass
     genPreprocessorOutput(infile, outfile)
     cleanPreprocessorOutput(outfile, infile)
-    runC2Nim(outfile)
+    runC2Nim(outfile, libfile_header)
     target_directory = os.path.dirname(nimfile).replace("source", "dist")
     os.makedirs(target_directory, exist_ok=True)
     subprocess.run(["cp", nimfile, target_directory])
